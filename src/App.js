a082541c120be5719963b2c25e01dd7d9249344d
Rio-Lv <wong.rogerio@gmail.com>
2021-01-18 21:54:25 +0000

    auth is working but i must fix the credential merge

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
 import Login from './components/Login';
-//import Logout from './components/Logout';
-import {firebase} from './config/firebase';
+
+import { firebase } from './config/firebase';
 import React, { useState } from 'react';
 
 function App() {
     const [user, setUser] = useState(null);
+    var provider = new firebase.auth.GoogleAuthProvider();
 
     firebase.auth().onAuthStateChanged((User) => {
         if (User) {
             // User is signed in.
-            setUser(User)
+            console.log('signing in')
+            setUser(User.displayName)
         } else {
             // No user is signed in.
-            setUser(null)
+            console.log('sign in failed')
         }
     });
 
@@ -21,16 +23,19 @@ function App() {
         <div className="App" >
             <h1>test functions</h1>
             <button onClick={() => {
-                user?
-                console.log(user.displayName)
-                :
-                console.log('no user found')
+                user ?
+                    console.log(user)
+                    :
+                    console.log('no user found')
             }}>who this?
             </button>
-            {/* <Logout/> */}
-
-            <Login/>
-            
+            <button onClick={() => {
+                firebase.auth().signInWithRedirect(provider);
+                const User = firebase.auth().currentUser;
+                setUser(User);
+                console.log(User);
+            }}>whhooooo</button>
+            <Login />
 
         </div>
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,23 @@
 import React from 'react'
-import { firebase, auth } from '../config/firebase';
 
+import { firebase,auth } from '../config/firebase';
 var firebaseui = require('firebaseui');
+
+// Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(auth);
 
 function Login() {
 
     ui.start('#firebaseui-auth-container', {
         signInOptions: [
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-            firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+          // List of OAuth providers supported.
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+          firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+          firebase.auth.GithubAuthProvider.PROVIDER_ID
         ],
         // Other config options...
-    });
+      });
     return (
         <div>
             <div id="firebaseui-auth-container"></div>
